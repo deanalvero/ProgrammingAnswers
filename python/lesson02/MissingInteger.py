416e2619ef123a1d87802ee13590b0fca156dc90
deanalvero <deanalvero@gmail.com>
2015-06-26 23:14:13 +0800

    Update MissingInteger.py

--- python/lesson02/MissingInteger.py
+++ python/lesson02/MissingInteger.py
@@ -4,15 +4,15 @@
 # https://codility.com/demo/take-sample-test/missing_integer/
 #
 def solution(A):
-	length = len(A)
-	B = [False] * (length + 1)
-	for i in xrange(length):
-		if A[i] > 0 and A[i] <= length and (not B[A[i] - 1]):
-			B[A[i] - 1] = True
-	for i in xrange(length + 1):
-		if B[i] == False:
-			return i + 1
-	return i + 1
+    length = len(A)
+    B = [False] * (length + 1)
+    for i in xrange(length):
+        if A[i] > 0 and A[i] <= length and (not B[A[i] - 1]):
+            B[A[i] - 1] = True
+    for i in xrange(length + 1):
+        if B[i] == False:
+            return i + 1
+    return i + 1
 	
 def main():
     print solution([1, 3, 6, 4, 1, 2])
