45e319cbe3ad21f0dbae185ce75b66dca6d69113
deanalvero <deanalvero@gmail.com>
2015-06-26 23:17:41 +0800

    Update TapeEquilibrium.py

--- python/lesson01/TapeEquilibrium.py
+++ python/lesson01/TapeEquilibrium.py
@@ -5,18 +5,18 @@ import sys
 # https://codility.com/demo/take-sample-test/tape_equilibrium/
 #
 def solution(A):
-	left = 0
-	right = sum(A)
-	result = sys.maxint
+    left = 0
+    right = sum(A)
+    result = sys.maxint
     
-	for i in range(0, len(A) - 1):
-		left += A[i]
-		right -= A[i]
-		result = min(result, abs(left - right))
-	return result
+    for i in range(0, len(A) - 1):
+        left += A[i]
+        right -= A[i]
+        result = min(result, abs(left - right))
+    return result
 
 def main():
-	print solution([3, 1, 2, 4, 3]);
+    print solution([3, 1, 2, 4, 3]);
 	
 if __name__ == "__main__":
-	main()
+    main()
