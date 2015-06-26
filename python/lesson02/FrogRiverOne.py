727873730d743259eda510a4d40f7e0c2edcebee
deanalvero <deanalvero@gmail.com>
2015-06-26 23:15:45 +0800

    Update FrogRiverOne.py

--- python/lesson02/FrogRiverOne.py
+++ python/lesson02/FrogRiverOne.py
@@ -6,14 +6,14 @@
 
 def solution(X, A):
     B = [-1] * (X + 1)
-	count = 0
-	for i in xrange(len(A)):
-		if B[A[i]] == -1:
-			B[A[i]] = i
-			count += 1
-			if count == X:
-				return i
-	return -1
+    count = 0
+    for i in xrange(len(A)):
+        if B[A[i]] == -1:
+            B[A[i]] = i
+            count += 1
+            if count == X:
+                return i
+    return -1
 
 def main():
     print solution(5, [1, 3, 1, 4, 2, 3, 5, 4])
