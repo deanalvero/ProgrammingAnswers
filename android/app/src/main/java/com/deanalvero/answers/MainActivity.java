260fb5c2fb4688a5b6538edd4f733a3e7d61b589
deanalvero <deanalvero@gmail.com>
2019-06-16 18:57:20 +0800

    java: add FlippingAnImage

--- android/app/src/main/java/com/deanalvero/answers/MainActivity.java
+++ android/app/src/main/java/com/deanalvero/answers/MainActivity.java
@@ -3,8 +3,6 @@ package com.deanalvero.answers;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.deanalvero.answers.exams.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
