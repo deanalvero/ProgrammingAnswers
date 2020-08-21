f2b866f6b02a95593887f818efc4509d7eb6d26e
deanalvero <deanalvero@gmail.com>
2020-08-20 22:30:44 -0400

    java: add IntegerToRoman, LRUCache, and MinimumPathSum

---
+++ android/lib/src/test/java/com/deanalvero/answers/leetcode/medium/IntegerToRomanTest.java
@@ -0,0 +1,50 @@
+package com.deanalvero.answers.leetcode.medium;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IntegerToRomanTest {
+    private IntegerToRoman testee;
+
+    @Before
+    public void setUp() {
+        testee = new IntegerToRoman();
+    }
+
+    @Test
+    public void intToRoman_inputExampleOne_returnCorrectOutput() {
+        String expected = "III";
+        String actual = testee.intToRoman(3);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void intToRoman_inputExampleTwo_returnCorrectOutput() {
+        String expected = "IV";
+        String actual = testee.intToRoman(4);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void intToRoman_inputExampleThree_returnCorrectOutput() {
+        String expected = "IX";
+        String actual = testee.intToRoman(9);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void intToRoman_inputExampleFour_returnCorrectOutput() {
+        String expected = "LVIII";
+        String actual = testee.intToRoman(58);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void intToRoman_inputExampleFive_returnCorrectOutput() {
+        String expected = "MCMXCIV";
+        String actual = testee.intToRoman(1994);
+        assertEquals(expected, actual);
+    }
+}
---
+++ android/lib/src/test/java/com/deanalvero/answers/leetcode/medium/LRUCacheTest.java
@@ -0,0 +1,23 @@
+package com.deanalvero.answers.leetcode.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LRUCacheTest {
+    private LRUCache testee;
+
+    @Test
+    public void operations_defaultInput_returnCorrectOutput() {
+        testee = new LRUCache( 2);
+        testee.put(1, 1);
+        testee.put(2, 2);
+        assertEquals(1, testee.get(1));
+        testee.put(3, 3);
+        assertEquals(-1, testee.get(2));
+        testee.put(4, 4);
+        assertEquals(-1, testee.get(1));
+        assertEquals(3, testee.get(3));
+        assertEquals(4, testee.get(4));
+    }
+}
